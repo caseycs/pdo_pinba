928c737490971beafb555e84a2d3dfcda4c7caf7
Ilia Kondrashov <caseycs@gmail.com>
2015-08-04 00:34:28 +0200

    more functional tests

--- test/PDOPinba/Test/PDOFunctionalTest.php
+++ test/PDOPinba/Test/PDOFunctionalTest.php
@@ -3,6 +3,12 @@ namespace PDOPinba\Test;
 
 class PDOFunctionalTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $pdo = new \PDO('mysql:dbname=pdo_pinba_test', 'travis');
+        $pdo->exec('DROP TABLE IF EXISTS test');
+    }
+
     public function testConnect()
     {
         new \PDOPinba\PDO('mysql:dbname=pdo_pinba_test', 'travis');
@@ -12,29 +18,39 @@ class PDOFunctionalTest extends \PHPUnit_Framework_TestCase
     {
         $pdo = new \PDOPinba\PDO('mysql:dbname=pdo_pinba_test', 'travis');
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $pdo->exec('CREATE TABLE IF NOT EXISTS test (id INT);');
+        $pdo->exec('CREATE TABLE test (id INT);');
         $pdo->beginTransaction();
         $pdo->exec('INSERT INTO test VALUES (10);');
         $pdo->commit();
+
+        $pdoRaw = new \PDO('mysql:dbname=pdo_pinba_test', 'travis');
+        $row = $pdoRaw->query('SELECT * FROM test')->fetch(\PDO::FETCH_ASSOC);
+        $this->assertSame(array('id' => 10), $row);
     }
 
     public function testTransactionRollback()
     {
         $pdo = new \PDOPinba\PDO('mysql:dbname=pdo_pinba_test', 'travis');
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $pdo->exec('CREATE TABLE IF NOT EXISTS test (id INT);');
+        $pdo->exec('CREATE TABLE test (id INT);');
         $pdo->beginTransaction();
         $pdo->exec('INSERT INTO test VALUES (10);');
         $pdo->commit();
+
+        $pdoRaw = new \PDO('mysql:dbname=pdo_pinba_test', 'travis');
+        $rows = $pdoRaw->query('SELECT * FROM test')->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertEmpty($rows);
     }
 
     public function testQueryAndStatement()
     {
         $pdo = new \PDOPinba\PDO('mysql:dbname=pdo_pinba_test', 'travis');
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $pdo->exec('CREATE TABLE IF NOT EXISTS test (id INT);');
+        $pdo->exec('CREATE TABLE test (id INT);');
         $pdo->exec('INSERT INTO test VALUES (10);');
-        $stmt = $pdo->query('SELECT * FROM test');
-        $this->assertNotEmpty($stmt->fetchAll());
+
+        $pdoRaw = new \PDO('mysql:dbname=pdo_pinba_test', 'travis');
+        $row = $pdoRaw->query('SELECT * FROM test')->fetch(\PDO::FETCH_ASSOC);
+        $this->assertSame(array('id' => 10), $row);
     }
 }
